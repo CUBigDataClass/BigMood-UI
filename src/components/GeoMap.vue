3ce1e6507ea234839e6f5839c10fa5a55fd90e28
Abigail Fernandes <abigailferns94@gmail.com>
2019-04-17 18:05:20 -0600

    Feature: Add World Map and modify datasource based on API response

--- src/components/GeoMap.vue
+++ src/components/GeoMap.vue
@@ -12,484 +12,11 @@
 
 <script>
 import axios from "axios";
-const dataSource = {
-  chart: {
-    caption: "Sentiment based on what's trending",
-    subcaption: "Click on a country to see trend stats",
-    numbersuffix: "%",
-    includevalueinlabels: "1",
-    labelsepchar: ": ",
-    entityfillhovercolor: "#FFF9C4",
-    theme: "candy"
-  },
-  colorrange: {
-    minvalue: "0",
-    code: "#FFE0B2",
-    gradient: "1",
-    color: [
-      {
-        minvalue: "0.5",
-        maxvalue: "1.0",
-        color: "#FFD74D"
-      },
-      {
-        minvalue: "1.0",
-        maxvalue: "2.0",
-        color: "#FB8C00"
-      },
-      {
-        minvalue: "2.0",
-        maxvalue: "3.0",
-        color: "#E65100"
-      }
-    ]
-  },
-  data: [
-    {
-      id: "NA",
-      value: ".82",
-      showlabel: "1",
-      link: "newchart-json-NAM"
-    },
-    {
-      id: "SA",
-      value: "2.04",
-      showlabel: "1",
-      link: "newchart-json-SAM"
-    },
-    {
-      id: "AS",
-      value: "1.78",
-      showlabel: "1",
-      link: "newchart-json-ASI"
-    },
-    {
-      id: "EU",
-      value: ".40",
-      showlabel: "1",
-      link: "newchart-json-EUP"
-    },
-    {
-      id: "AF",
-      value: "2.58",
-      showlabel: "1",
-      link: "newchart-json-AFC"
-    },
-    {
-      id: "AU",
-      value: "1.30",
-      showlabel: "1",
-      link: "newchart-json-AUS"
-    }
-  ],
-  linkeddata: [
-    {
-      id: "NAM",
-      linkedchart: {
-        chart: {
-          caption: "Average Annual Population Growth - North America",
-          subcaption: "1955 - 2015",
-          yaxisname: "Growth",
-          numbersuffix: "%",
-          palettecolors: "FFD74D",
-          theme: "fusion"
-        },
-        data: [
-          {
-            label: "1955",
-            value: "1.5078"
-          },
-          {
-            label: "1960",
-            value: "1.5502"
-          },
-          {
-            label: "1965",
-            value: "1.3121"
-          },
-          {
-            label: "1970",
-            value: "0.8648"
-          },
-          {
-            label: "1975",
-            value: "0.6402"
-          },
-          {
-            label: "1980",
-            value: "0.62"
-          },
-          {
-            label: "1985",
-            value: "0.6748"
-          },
-          {
-            label: "1990",
-            value: "0.6882"
-          },
-          {
-            label: "1995",
-            value: "0.6804"
-          },
-          {
-            label: "2000",
-            value: "0.5627"
-          },
-          {
-            label: "2005",
-            value: "0.5373"
-          },
-          {
-            label: "2010",
-            value: "0.5536"
-          },
-          {
-            label: "2015",
-            value: "0.4291"
-          }
-        ]
-      }
-    },
-    {
-      id: "SAM",
-      linkedchart: {
-        chart: {
-          caption: "Average Annual Population Growth - South America",
-          subcaption: "1955 - 2015",
-          yaxisname: "Growth",
-          numbersuffix: "%",
-          palettecolors: "E65100",
-          theme: "fusion"
-        },
-        data: [
-          {
-            label: "1955",
-            value: "2.6275"
-          },
-          {
-            label: "1960",
-            value: "2.6995"
-          },
-          {
-            label: "1965",
-            value: "2.757"
-          },
-          {
-            label: "1970",
-            value: "2.5376"
-          },
-          {
-            label: "1975",
-            value: "2.3431"
-          },
-          {
-            label: "1980",
-            value: "2.3261"
-          },
-          {
-            label: "1985",
-            value: "2.2036"
-          },
-          {
-            label: "1990",
-            value: "1.9611"
-          },
-          {
-            label: "1995",
-            value: "1.7184"
-          },
-          {
-            label: "2000",
-            value: "1.5965"
-          },
-          {
-            label: "2005",
-            value: "1.4482"
-          },
-          {
-            label: "2010",
-            value: "1.2031"
-          },
-          {
-            label: "2015",
-            value: "1.0698"
-          }
-        ]
-      }
-    },
-    {
-      id: "ASI",
-      linkedchart: {
-        chart: {
-          caption: "Average Annual Population Growth - Asia",
-          subcaption: "1955 - 2015",
-          yaxisname: "Growth",
-          numbersuffix: "%",
-          theme: "fusion",
-          palettecolors: "FB8C00"
-        },
-        data: [
-          {
-            label: "1955",
-            value: "1.9075"
-          },
-          {
-            label: "1960",
-            value: "1.8842"
-          },
-          {
-            label: "1965",
-            value: "2.1082"
-          },
-          {
-            label: "1970",
-            value: "2.4554"
-          },
-          {
-            label: "1975",
-            value: "2.3036"
-          },
-          {
-            label: "1980",
-            value: "1.9889"
-          },
-          {
-            label: "1985",
-            value: "1.9683"
-          },
-          {
-            label: "1990",
-            value: "2.0176"
-          },
-          {
-            label: "1995",
-            value: "1.6823"
-          },
-          {
-            label: "2000",
-            value: "1.3682"
-          },
-          {
-            label: "2005",
-            value: "1.2435"
-          },
-          {
-            label: "2010",
-            value: "1.1661"
-          },
-          {
-            label: "2015",
-            value: "1.0731"
-          }
-        ]
-      }
-    },
-    {
-      id: "EUP",
-      linkedchart: {
-        chart: {
-          caption: "Average Annual Population Growth - Europe",
-          subcaption: "1955 - 2015",
-          yaxisname: "Growth",
-          numbersuffix: "%",
-          theme: "fusion",
-          palettecolors: "FFE0B2"
-        },
-        data: [
-          {
-            label: "1955",
-            value: "1.026"
-          },
-          {
-            label: "1960",
-            value: "1.0652"
-          },
-          {
-            label: "1965",
-            value: "0.9381"
-          },
-          {
-            label: "1970",
-            value: "0.6925"
-          },
-          {
-            label: "1975",
-            value: "0.54"
-          },
-          {
-            label: "1980",
-            value: "0.4218"
-          },
-          {
-            label: "1985",
-            value: "0.354"
-          },
-          {
-            label: "1990",
-            value: "0.2971"
-          },
-          {
-            label: "1995",
-            value: "0.0276"
-          },
-          {
-            label: "2000",
-            value: "-0.1301"
-          },
-          {
-            label: "2005",
-            value: "-0.1558"
-          },
-          {
-            label: "2010",
-            value: "-0.0576"
-          },
-          {
-            label: "2015",
-            value: "-0.0292"
-          }
-        ]
-      }
-    },
-    {
-      id: "AFC",
-      linkedchart: {
-        chart: {
-          caption: "Average Annual Population Growth - Africa",
-          subcaption: "1955 - 2015",
-          yaxisname: "Growth",
-          numbersuffix: "%",
-          theme: "fusion",
-          palettecolors: "E65100"
-        },
-        data: [
-          {
-            label: "1955",
-            value: "2.1242"
-          },
-          {
-            label: "1960",
-            value: "2.338"
-          },
-          {
-            label: "1965",
-            value: "2.5075"
-          },
-          {
-            label: "1970",
-            value: "2.5947"
-          },
-          {
-            label: "1975",
-            value: "2.7175"
-          },
-          {
-            label: "1980",
-            value: "2.8398"
-          },
-          {
-            label: "1985",
-            value: "2.8857"
-          },
-          {
-            label: "1990",
-            value: "2.8243"
-          },
-          {
-            label: "1995",
-            value: "2.6172"
-          },
-          {
-            label: "2000",
-            value: "2.5072"
-          },
-          {
-            label: "2005",
-            value: "2.4853"
-          },
-          {
-            label: "2010",
-            value: "2.5593"
-          },
-          {
-            label: "2015",
-            value: "2.6001"
-          }
-        ]
-      }
-    },
-    {
-      id: "AUS",
-      linkedchart: {
-        chart: {
-          caption: "Average Annual Population Growth - Oceania",
-          subcaption: "1955 - 2015",
-          yaxisname: "Growth",
-          numbersuffix: "%",
-          theme: "candy",
-          palettecolors: "FB8C00"
-        },
-        data: [
-          {
-            label: "1955",
-            value: "1.511"
-          },
-          {
-            label: "1960",
-            value: "1.6045"
-          },
-          {
-            label: "1965",
-            value: "1.5578"
-          },
-          {
-            label: "1970",
-            value: "1.455"
-          },
-          {
-            label: "1975",
-            value: "1.4727"
-          },
-          {
-            label: "1980",
-            value: "1.2404"
-          },
-          {
-            label: "1985",
-            value: "1.2398"
-          },
-          {
-            label: "1990",
-            value: "1.1853"
-          },
-          {
-            label: "1995",
-            value: "1.2006"
-          },
-          {
-            label: "2000",
-            value: "1.1244"
-          },
-          {
-            label: "2005",
-            value: "1.0724"
-          },
-          {
-            label: "2010",
-            value: "1.1255"
-          },
-          {
-            label: "2015",
-            value: "1.0397"
-          }
-        ]
-      }
-    }
-  ]
-}
+import countryIdsFusionCharts from "../data/fusionChartsWorldIds.json";
 export default {
   name: "GeoMap",
   props: {},
+
   data: function() {
     return {
       headers: [
@@ -500,11 +27,83 @@ export default {
         // { text: "Woeid", value: "Woeid" }
       ],
       trends: [],
-      width: "50%",
-      height: "400",
-      type: "world",
+      width: "70%",
+      height: "500",
+      type: "worldwithcountries",
       dataFormat: "json",
-      dataSource
+      dataSource: {
+        chart: {
+          caption: "Sentiment based on what's trending",
+          subcaption: "Click on a country to see trend stats",
+          numbersuffix: "%",
+          includevalueinlabels: "1",
+          labelsepchar: ": ",
+          entityfillhovercolor: "#FFF9C4",
+          theme: "candy"
+        },
+        colorrange: {
+          minvalue: "0",
+          code: "#FFE0B2",
+          gradient: "1",
+          color: [
+            {
+              minvalue: "0.5",
+              maxvalue: "1.0",
+              color: "#FFD74D"
+            },
+            {
+              minvalue: "1.0",
+              maxvalue: "2.0",
+              color: "#FB8C00"
+            },
+            {
+              minvalue: "2.0",
+              maxvalue: "3.0",
+              color: "#E65100"
+            }
+          ]
+        },
+        data: [
+          {
+            id: "NA",
+            value: ".82",
+            showlabel: "1",
+            link: "newchart-json-NAM"
+          },
+          {
+            id: "23",
+            value: "2.04",
+            displayvalue: 'Hi',
+            tooltext: "USA{br}Trend: #MarchMadness {br}TweetVolume: 1000",
+            // showlabel: "1",
+            link: "newchart-json-SAM"
+          },
+          {
+            id: "104",
+            value: "0.5",
+            // showlabel: "1",
+            link: "newchart-json-ASI"
+          },
+          {
+            id: "IN",
+            value: ".40",
+            showlabel: "1",
+            link: "newchart-json-EUP"
+          },
+          {
+            id: "AF",
+            value: "2.58",
+            showlabel: "1",
+            link: "newchart-json-AFC"
+          },
+          {
+            id: "AU",
+            value: "1.30",
+            showlabel: "1",
+            link: "newchart-json-AUS"
+          }
+        ]
+      }
     };
   },
 
@@ -514,6 +113,22 @@ export default {
       .then(response => {
         // console.log(response.data.trends);
         this.trends = response.data.trends;
+        const data = [];
+
+        // Country trends
+        let trends = this.trends.filter(
+          trend => trend.locationType == "Country"
+        );
+        trends.forEach(trend => {
+          const trendingHashtag = trend.twitterTrendInfo[0];
+          let obj = {
+            id: countryIdsFusionCharts[trend.countryCode],
+            value: trendingHashtag.sentiment,
+            tooltext: trend.country + "{br}Trend:" + trendingHashtag.name + "{br}Tweet Volume: " + trendingHashtag.tweetVolume,
+            link: ""
+          };
+          data.push(obj);
+        });
       })
       .catch(error => {
         this.error = error;
--- src/main.js
+++ src/main.js
@@ -7,13 +7,22 @@ import Charts from 'fusioncharts/fusioncharts.charts';
 import CandyTheme from 'fusioncharts/themes/fusioncharts.theme.candy';
 import Maps from 'fusioncharts/fusioncharts.maps';
 import World from 'fusioncharts/maps/fusioncharts.world';
+import WorldWithCountries from 'fusioncharts/maps/fusioncharts.worldwithcountries';
 import 'vuetify/dist/vuetify.min.css';
 
 Vue.use(Vuetify);
-Vue.use(VueFusionCharts, FusionCharts, Charts, CandyTheme, Maps, World);
+Vue.use(
+  VueFusionCharts,
+  FusionCharts,
+  Charts,
+  CandyTheme,
+  Maps,
+  World,
+  WorldWithCountries,
+);
 
 Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App)
+  render: h => h(App),
 }).$mount('#app');
